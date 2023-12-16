76d45c15c053681b73b400cfd8ddbffb476faa51
seokmyungham <hellotjraud@gmail.com>
2023-12-16 15:55:14 +0900

    feat(InputValidator): 클래스 생성 및 validateMonthFormat, validateDayOfWeekFormat 메서드 추가

--- src/main/java/oncall/util/InputValidator.java
+++ src/main/java/oncall/util/InputValidator.java
@@ -1,8 +1,22 @@
 package oncall.util;
 
+import static oncall.constant.ErrorMessage.INVALID_INPUT_ERROR_MESSAGE;
+
 import java.util.List;
 
 public class InputValidator {
     private static final List<String> MONTH_FORMAT = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
-    private static final List<String> WEEK_OF_DAY_FORMAT = List.of("월", "화", "수", "목", "금", "토", "일");
+    private static final List<String> DAY_OF_WEEK_FORMAT = List.of("월", "화", "수", "목", "금", "토", "일");
+
+    public static void validateMonthFormat(String month) {
+        if (!MONTH_FORMAT.contains(month)) {
+            throw new IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateDayOfWeekFormat(String dayOfWeek) {
+        if (!DAY_OF_WEEK_FORMAT.contains(dayOfWeek)) {
+            throw new IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE);
+        }
+    }
 }
