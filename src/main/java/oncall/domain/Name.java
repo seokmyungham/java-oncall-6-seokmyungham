2c6345031853bb18bc2e665ba7fc75be6b04ed09
seokmyungham <hellotjraud@gmail.com>
2023-12-16 14:15:51 +0900

    feat(Name): validateNameLength 메서드 추가
    validateNameLength: 닉네임의 길이를 검증하는 메서드입니다

--- src/main/java/oncall/domain/Name.java
+++ src/main/java/oncall/domain/Name.java
@@ -1,9 +1,18 @@
 package oncall.domain;
 
+import static oncall.constant.ErrorMessage.NAME_LENGTH_ERROR_MESSAGE;
+
 public class Name {
     private final String name;
 
     public Name(String name) {
+        validateNameLength(name);
         this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE);
+        }
     }
 }
