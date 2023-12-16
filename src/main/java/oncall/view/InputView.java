d47e2b280f9e94832b7e03a23966b6e87ae77b50
seokmyungham <hellotjraud@gmail.com>
2023-12-16 15:00:54 +0900

    feat(InputView): 클래스 생성 및 메서드 추가
    inputMonthAndDayOfWeek: 달과 시작 요일을 입력받는 메서드입니다
    inputWeekdayMembers: 평일 근무 사원 순번 닉네임을 입력 받습니다
    inputDayOffMembers: 휴일 근무 사원 순번 닉네임을 입력 받습니다

---
+++ src/main/java/oncall/view/InputView.java
@@ -0,0 +1,25 @@
+package oncall.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static final String MONTH_AND_DAY_OF_WEEK_INPUT_PROMPT = "비상 근무를 배정할 월과 시작 요일을 입력하세요> ";
+    public static final String WEEKDAY_MEMBERS_INPUT_PROMPT = "%n평일 비상 근무 순번대로 사원 닉넹미을 입력하세요> ";
+    public static final String DAY_OFF_MEMBERS_INPUT_PROMPT = "%n휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ";
+
+    public String inputMonthAndDayOfWeek() {
+        System.out.printf(MONTH_AND_DAY_OF_WEEK_INPUT_PROMPT);
+        return Console.readLine();
+    }
+
+    public String inputWeekdayMembers() {
+        System.out.printf(WEEKDAY_MEMBERS_INPUT_PROMPT);
+        return Console.readLine();
+    }
+
+    public String inputDayOffMembers() {
+        System.out.printf(DAY_OFF_MEMBERS_INPUT_PROMPT);
+        return Console.readLine();
+    }
+}
