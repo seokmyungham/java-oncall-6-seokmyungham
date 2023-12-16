b778e052e653d272a2f98d4c4f24edfdc2b44557
seokmyungham <hellotjraud@gmail.com>
2023-12-16 17:49:59 +0900

    refactor(OnCallService): service 메서드 리팩토링

--- src/main/java/oncall/service/OnCallService.java
+++ src/main/java/oncall/service/OnCallService.java
@@ -10,41 +10,50 @@ import oncall.domain.DayOff;
 import oncall.domain.Name;
 
 public class OnCallService {
-
     public Map<Integer, Name> service(LinkedList<Name> weekdayMembers, LinkedList<Name> dayOffMembers, List<Integer> dayOffs, int month) {
         Map<Integer, Name> result = new HashMap<>();
-
         YearMonth yearMonth = YearMonth.of(2023, month);
-
-        for (int i = 1; i <= yearMonth.lengthOfMonth(); i++) {
-            if (dayOffs.contains(i)) {
-                Name member = dayOffMembers.poll();
-
-                if (i != 1 && result.get(i - 1).equals(member)) {
-                    Name switchedMember = dayOffMembers.poll();
-                    dayOffMembers.addFirst(member);
-                    result.put(i, switchedMember);
-                    dayOffMembers.addLast(switchedMember);
-                    continue;
-                }
-                dayOffMembers.addLast(member);
-                result.put(i, member);
+        for (int day = 1; day <= yearMonth.lengthOfMonth(); day++) {
+            boolean isDayOff = dayOffs.contains(day);
+            if (isDayOff) {
+                dayOffProcess(day, dayOffMembers, result);
             }
-            if (!dayOffs.contains(i)) {
-                Name member = weekdayMembers.poll();
-
-                if (i != 1 && result.get(i - 1).equals(member)) {
-                    Name switchedMember = weekdayMembers.poll();
-                    weekdayMembers.addFirst(member);
-                    result.put(i, switchedMember);
-                    weekdayMembers.addLast(switchedMember);
-                    continue;
-                }
-                weekdayMembers.addLast(member);
-                result.put(i, member);
+            if (!isDayOff) {
+                weekdayProcess(day, weekdayMembers, result);
             }
         }
         return result;
+    }
+
+    private void dayOffProcess(int day, LinkedList<Name> dayOffMembers, Map<Integer, Name> result) {
+            Name member = dayOffMembers.poll();
+            if (isWorkedJustBefore(day, result, member)) {
+                switchMembers(dayOffMembers, member, result, day);
+                return;
+            }
+            dayOffMembers.addLast(member);
+            result.put(day, member);
+    }
+
+    private void weekdayProcess(int day, LinkedList<Name> weekdayMembers, Map<Integer, Name> result) {
+            Name member = weekdayMembers.poll();
+            if (isWorkedJustBefore(day, result, member)) {
+                switchMembers(weekdayMembers, member, result, day);
+                return;
+            }
+            weekdayMembers.addLast(member);
+            result.put(day, member);
+    }
+
+    private boolean isWorkedJustBefore(int day, Map<Integer, Name> result, Name member) {
+        return day != 1 && result.get(day - 1).equals(member);
+    }
+
+    private void switchMembers(LinkedList<Name> members, Name member, Map<Integer, Name> result, int day) {
+        Name switchedMember = members.poll();
+        members.addFirst(member);
+        result.put(day, switchedMember);
+        members.addLast(switchedMember);
     }
 
     public List<Integer> getDayOffs(int month, DayOfWeek startDayOfWeek) {
