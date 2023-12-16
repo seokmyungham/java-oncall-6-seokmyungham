4de459234db25f919efdf4e9e46ede72ac409485
seokmyungham <hellotjraud@gmail.com>
2023-12-16 15:50:26 +0900

    feat(InputValidator): 클래스 생성

---
+++ src/main/java/oncall/util/InputValidator.java
@@ -0,0 +1,8 @@
+package oncall.util;
+
+import java.util.List;
+
+public class InputValidator {
+    private static final List<String> MONTH_FORMAT = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
+    private static final List<String> WEEK_OF_DAY_FORMAT = List.of("월", "화", "수", "목", "금", "토", "일");
+}
