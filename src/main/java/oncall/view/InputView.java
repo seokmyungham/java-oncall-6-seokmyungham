971847434e2343ec1b1f560248a78984ae01596f
seokmyungham <hellotjraud@gmail.com>
2023-12-16 17:28:28 +0900

    refactor(InputView): 상수 메시지 수정

--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -5,8 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static final String MONTH_AND_DAY_OF_WEEK_INPUT_PROMPT = "비상 근무를 배정할 월과 시작 요일을 입력하세요> ";
-    public static final String WEEKDAY_MEMBERS_INPUT_PROMPT = "%n평일 비상 근무 순번대로 사원 닉넹미을 입력하세요> ";
-    public static final String DAY_OFF_MEMBERS_INPUT_PROMPT = "%n휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ";
+    public static final String WEEKDAY_MEMBERS_INPUT_PROMPT = "평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ";
+    public static final String DAY_OFF_MEMBERS_INPUT_PROMPT = "휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ";
 
     public String inputMonthAndDayOfWeek() {
         System.out.printf(MONTH_AND_DAY_OF_WEEK_INPUT_PROMPT);
