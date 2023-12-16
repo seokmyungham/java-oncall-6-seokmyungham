ebfb5857ee3bc36462a269c6cb035891f5ec2dad
seokmyungham <hellotjraud@gmail.com>
2023-12-16 16:16:22 +0900

    refactor(Member): turnNum 필드 삭제

--- src/main/java/oncall/domain/Member.java
+++ src/main/java/oncall/domain/Member.java
@@ -2,10 +2,8 @@ package oncall.domain;
 
 public class Member {
     private final Name name;
-    private final int turnNum;
 
-    public Member(Name name, int turnNum) {
+    public Member(Name name) {
         this.name = name;
-        this.turnNum = turnNum;
     }
 }
