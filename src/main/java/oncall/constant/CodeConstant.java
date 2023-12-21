81f244235b9ac6190399fe1358c354a37d386ca8
seokmyungham <hellotjraud@gmail.com>
2023-12-21 12:41:42 +0900

    feat(CodeConstant): 상수를 모아놓은 클래스 생성

---
+++ src/main/java/oncall/constant/CodeConstant.java
@@ -0,0 +1,12 @@
+package oncall.constant;
+
+public class CodeConstant {
+    public static final String WEEKDAY_CODE = "WEEKDAY";
+    public static final String DAY_OFF_CODE = "DAYOFF";
+
+    public static final int INDEX_ZERO = 0;
+    public static final int INDEX_ONE = 1;
+
+    public static final String DELIMITER_COMMA = ",";
+
+}
